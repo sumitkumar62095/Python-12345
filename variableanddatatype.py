75f43766c8ac2c7a924a6d5a22d3495891b4d5b5
sumitindixpert <sumikumar74htce@gmail.com>
2026-02-09 00:39:41 +0530

    variable

---
+++ variableanddatatype.py
@@ -0,0 +1,111 @@
+#======variable and data type=========#
+#======integer======#
+
+
+#======type conversion=======#
+
+marks=int(input("enter number:"))
+marks2=30
+
+print(marks==marks2)
+
+#=======type casting=======#
+
+
+a=int("2")
+b=2.35
+
+print(a+b)
+
+
+a=10
+b=20
+
+print(a+b)
+
+first_number=int(input("enter number:"))
+secoand_number=int(input("enter number:"))
+total_number=first_number+secoand_number
+
+print("enter number:",first_number)
+print("enter number:",secoand_number)
+
+print("totalnumber",total_number)
+
+
+#================================================
+
+hindi=int(input("please enter hindi marks:"))
+english=int(input("please enter english marks:"))
+maths=int(input("please enter english marks:"))
+science=int(input("please enter science marks:"))
+computer=int(input("please enter computer marks:"))
+
+total_marks=hindi+english+maths+science+computer
+percentage=total_marks/5
+
+print(f"user enter bhindi marks:{hindi}")
+print(f"user enter english marks:{english}")
+print(f"user enter maths marks{maths}")
+print(f"user enter science nmarks:{science}")
+print(f"user enter computer mareks:{computer}")
+print(f"user total marks:{total_marks}")
+print(f"total percentage:{percentage}")
+
+#==================================
+
+
+hindi=int(input("please enter your hindi marks:"))
+english=int(input("please enter your english marks:"))
+maths=int(input("please enter your maths marks:"))
+
+total_marks=hindi+english+maths
+
+
+print("hindi marks:",hindi)
+print("english marks:",english)
+print("maths marks:",maths)
+
+print("total marks:",total_marks)
+
+#=====================================
+
+
+name="sumitkumar"
+age=20
+float=12.4
+
+
+print(type(name))
+print(type(age))
+print(type(float))
+
+
+#========float========#
+
+a=2.4
+b=3.4
+
+print(a+b)
+
+
+number=float(input("enter number:"))
+number2=float(input("enter number:"))
+
+total_number=number+number2
+
+print("enter number:",number)
+print("enter number:",number2)
+
+print(total_number)
+
+#====complexnumber=====#
+
+
+a=2+3j+4j+5j
+b=30+4j
+
+print(a)
+print(type(a))
+
+
