0cec28442faaf5006918bfcf33375c5dfa5beb9c
sumitindixpert <sumikumar74htce@gmail.com>
2026-02-09 00:38:19 +0530

    while

---
+++ whileloop.py
@@ -0,0 +1,274 @@
+# ====================while loop ==========================
+
+while count <= 5:
+
+
+    print("sumit")
+
+    count +=1
+
+#==========
+
+number=int(input("please enter your number:"))
+
+i=1
+while i<=10:
+
+    print(number*i)
+
+    i+=1
+
+
+
+#=============
+
+i=1
+
+while i<=20:
+
+    if i==10:
+
+        break
+
+    print(i)
+
+    i+=1
+
+
+
+
+#=================================
+
+number=int(input("please enter your number:"))
+i=1
+
+while i<=10:
+
+    print(number*i)
+
+    i+=1
+
+
+
+
+#=========================
+i=1
+
+while i<=10:
+
+    print("sumit")
+
+    i+=1
+# # =====================
+# i=1
+
+while i<=10:
+    print(i)
+
+    i+=1
+
+#============================
+i=10
+
+while i>=1:
+
+    print(i)
+
+    i-=1
+
+#================================
+
+n=int(input("please enter your number:"))
+
+i=1
+
+while i<=10:
+
+    print(n*i)
+
+    i+=1
+
+#============================
+
+
+name=["sumit kumar","ravi kumar","rohan kumar"]
+
+
+i=0
+
+while i<len(name):
+
+    print(name[i])
+
+    i+=1
+
+
+#======================
+i=1
+while i<=50:
+
+    if i%2==0:
+        print(i)
+
+        i+=1
+
+
+
+#==========================
+
+
+address=["sumit kumar","ravi kumar"]
+
+i=0
+
+while i<len(address):
+
+    print(address[i])
+
+    i+=1
+# #================jamp statemet=========
+
+#==============break statement================
+
+i=1
+
+while i<=10:
+
+    print(i)
+
+    if i==3:
+
+        break
+
+    i+=1
+
+#=====================
+
+#============continue============
+i=1
+
+while i<=10:
+
+    i+=1
+
+    if i==5:
+
+        continue
+
+    print(i)
+
+    i+=1
+
+
+
+
+# #===========================
+i=1
+
+while i<=10:
+
+    if(i%2==0):
+
+        i+-1
+
+        continue
+    print(i)
+
+    i+=1
+
+#=====================================
+number=[2,3,4,6,8,94,5,7,3]
+
+i=1
+while i<len(number):
+
+    if i%2==0:
+        print(i)
+
+        i+=1
+
+
+
+address=("sunday","monday","tuesday","wednesday","thrusday","friday")
+
+
+i=1
+
+while i<len(address):
+
+    print(address[i])
+
+    i+=1
+
+#=============================
+
+number=int(input("please enter your number:"))
+
+i=1
+while i<=number:
+
+
+    print(i)
+
+    i+=1
+
+
+#============================================
+
+
+while True:
+
+    number=int(input("please enter your number:"))
+
+    if number%2==0   and  number%3==0:
+
+        print("you are right even number")
+
+        break
+
+    else:
+
+        print("rong number please tray again")
+        
+#===============================
+
+number=int(input("please enter your number:"))
+
+i=1
+
+while i<=number:
+
+    print(i)
+
+    i+=1
+
+#==========================================
+
+
+while True:
+
+    number=int(input("please enter your number:"))
+
+    i=1
+
+    while i<=number:
+
+        print(i)
+
+        i+=1
+
+#==================================
+
+while True:
+
+    number=int(input("please enter your number:"))
+    
+
+
+
+    if number==0:
+
+        break
+
+
+
